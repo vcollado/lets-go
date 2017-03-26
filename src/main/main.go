d6d7bed6839bab143e71bcdad2fed6f6ba957ded
vcollado <necseron@gmail.com>
2017-03-26 19:21:04 +0200

    Added the arguments to detect client and host

--- src/main/main.go
+++ src/main/main.go
@@ -1,4 +1,31 @@
 package main
 
+import (
+	"flag"
+	"os"
+)
+
 func main() {
+	var isHost bool
+
+	flag.BoolVar(&isHost, "listen", false, "Listens of the specified ip address")
+	flag.Parse()
+
+	if isHost {
+		// go run main-go -listen <ip>
+		connIP := os.Args[2]
+		runHost(connIP)
+	} else {
+		// go run main.go <ip>
+		connIP := os.Args[1]
+		runGuest(connIp)
+	}
+}
+
+func runHost(ip string) {
+
+}
+
+func runGuest(ip string) {
+
 }
