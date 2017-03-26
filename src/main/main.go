3bd7958a81fc278e9817ea4571c41af376a9b893
vcollado <vcollado@lenovo>
2017-03-26 19:03:57 +0200

    code cleaning to start coding the chat from scratch

--- src/main/main.go
+++ src/main/main.go
@@ -1,47 +1,4 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-	"os"
-	"time"
-)
-
 func main() {
-
-	hourOfDay := time.Now().Hour()
-	greeting, err := getGreeting(hourOfDay)
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	fmt.Println(greeting)
-
-	args := os.Args
-	if len(args) > 1 {
-		fmt.Println(args[1])
-	} else {
-		fmt.Println("Hello, Gopher")
-	}
-}
-
-func getGreeting(hour int) (string, error) {
-
-	var message string
-
-	if hour < 7 {
-		err := errors.New("Too early for greetings!")
-		return message, err
-	}
-
-	if hour < 12 {
-		message = "Good Morning"
-	} else if hour < 18 {
-		message = "Good Afternoon"
-	} else {
-		message = "Good Evening"
-	}
-	return message, nil
 }
